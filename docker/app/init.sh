8783b3f4fc213f54cb9ee18c702c7c3409c0f78a
SeekerRook <xrist.vardakis@gmail.com>
2023-01-14 04:05:35 +0200

    added ui

---
+++ docker/app/init.sh
@@ -0,0 +1,2 @@
+# python -m http.server &
+uvicorn app.main:app --host 0.0.0.0 --port 1333
--- docker/app/main.py
+++ docker/app/main.py
@@ -1,9 +1,71 @@
 from fastapi import FastAPI
 from .triton import run
+# from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+app = FastAPI()
 
+# origins = [
+#     "http://localhost",
+#     "http://localhost:8000",
+#     "172.17.0.1"
+#     "127.0.0.1"
+# ]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/triton-api")
 def read_root():
     return run()
+@app.get("/" ,response_class=HTMLResponse)
+def read_page():
+    data = run()
+    return """
+<html lang="en">
+<head>
+<title>TRITON</title>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <link rel="shortcut icon" href="#">
+</head>
+<style>
+    table, th, td {
+      border:1px solid rgb(0, 151, 151);
+    }
+    body{
+    background: rgb(39, 40, 41);
+    color: rgb(255, 255, 255);
+    }
+    h1, h2 {
+        color: rgb(0, 195, 255);
+    }
+    </style>"""+  f"""
+<body>
+           <h1>TRITON</h1>
+        <h2>GAIN : ${data["Gain"]}€</h2>
+        <table >
+  <tr>
+    <th>Current Unit Price </th>
+    <th> Value (705.864 units)</th>
+    <th>Clear Value (- 160 fee)</th>
+    <th>Gain</th>
+    <th>Gain %</th>
+  </tr>
+  <tr>
+    <td>${data['current unit Price']}€</td>
+    <td>${data['Value (705.864 units)']}€</td>
+    <td>${data['Clear_Value (- 160 fee)']}€</td>
+    <td>${data['Gain']}€</td>
+    <td> ${data['Gain_%']}%</td>
+  </tr>
+
+</table>
+</body>
+    </html>
+    """
